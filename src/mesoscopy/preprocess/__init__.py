c788f71bfb5ca794cc25750ac55f7d2011cd9af1
Constantinos Eleftheriou <Constantinos.Eleftheriou@ed.ac.uk>
2022-04-12 21:53:29 +0100

    crop before binning

--- src/mesoscopy/preprocess/__init__.py
+++ src/mesoscopy/preprocess/__init__.py
@@ -69,6 +69,10 @@ def preprocess(raw_path, out_dir, chunks=100, channel_means_only=False, crop=0):
 
     raw_frames = da.from_array(d, chunks=(chunks, d.shape[1], d.shape[2]))
 
+    if crop > 0:
+        raw_frames = raw_frames[:, crop:-crop, crop:-crop]
+        raw_frames.rechunk(chunks=(chunks, raw_frames.shape[1], raw_frames.shape[2]))
+
     # 2x2 binning
     raw_frames = raw_frames.reshape(
         d.shape[0],
@@ -79,10 +83,6 @@ def preprocess(raw_path, out_dir, chunks=100, channel_means_only=False, crop=0):
         d.shape[2] // (d.shape[2] / 2),
     ).mean(axis=(-1, 1, 3))
     click.echo("2x2 binning to shape {}".format(raw_frames.shape))
-
-    if crop > 0:
-        raw_frames = raw_frames[:, crop:-crop, crop:-crop]
-        raw_frames.rechunk(chunks=(chunks, raw_frames.shape[1], raw_frames.shape[2]))
 
     # Channel separation
     # Get the global mean and std values for each frame
