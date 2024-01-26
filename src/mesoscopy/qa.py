49e2fac59778ee32e2b77cf4a1c62e7d64b28a52
Constantinos Eleftheriou <Constantinos.Eleftheriou@ed.ac.uk>
2024-01-26 17:15:49 +0000

    refactor all qa plots out of preprocessing module

---
+++ src/mesoscopy/qa.py
@@ -0,0 +1,35 @@
+import click
+
+from matplotlib import pyplot as plt
+
+
+def plot_line(array, outpath, message=None):
+    plt.clf()
+    plt.plot(array)
+    plt.savefig(outpath)
+    if message:
+        click.echo(message)
+
+
+def plot_lines(arrays, outpath, message=None):
+    plt.clf()
+    for array in arrays:
+        plt.plot(array)
+    plt.savefig(outpath)
+    if message:
+        click.echo(message)
+
+
+def plot_hist(array, outpath, message=None):
+    plt.clf()
+    plt.hist(array)
+    plt.savefig(outpath)
+    if message:
+        click.echo(message)
+
+
+def plot_frame(array, outpath, message=None):
+    plt.clf()
+    plt.imsave(outpath, array)
+    if message:
+        click.echo(message)
