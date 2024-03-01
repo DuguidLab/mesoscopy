5569d6a1b6e5d2157bc495ebf6ca262048f378fa
Constantinos Eleftheriou <Constantinos.Eleftheriou@ed.ac.uk>
2024-03-01 15:45:15 +0000

    refactor preprocessing __init__ to use separate modules, rename non-cli functions to be privates

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -4,4 +4,4 @@ import mesoscopy.preprocess as preprocess
 
 
 def test_nwb_link(nwbfile, preproc_h5):
-    preprocess.update_nwb(nwbfile, preproc_h5)
+    preprocess._update_nwb(nwbfile, preproc_h5)
