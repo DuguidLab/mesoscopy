d0fb1475a686c76af4360da57b06b11a43026ec1
Constantinos Eleftheriou <Constantinos.Eleftheriou@ed.ac.uk>
2024-03-05 14:31:59 +0000

    add scatterplots

--- src/mesoscopy/plots.py
+++ src/mesoscopy/plots.py
@@ -33,3 +33,24 @@ def plot_frame(array, outpath, message=None):
     plt.imsave(outpath, array)
     if message:
         click.echo(message)
+
+
+def plot_scatter(x, y, outpath, labels=[], message=None):
+    plt.clf()
+    plt.scatter(x, y)
+    if labels:
+        plt.legend(labels)
+    plt.savefig(outpath)
+    if message:
+        click.echo(message)
+
+
+def plot_scatters(xs, ys, outpath, labels=[], message=None):
+    plt.clf()
+    for x, y in zip(xs, ys):
+        plt.scatter(x, y)
+    if labels:
+        plt.legend(labels)
+    plt.savefig(outpath)
+    if message:
+        click.echo(message)
