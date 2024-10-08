c2f85fd70ccda3b9a78b89437cd30232a7242a16
Constantinos Eleftheriou <Constantinos.Eleftheriou@ed.ac.uk>
2024-10-08 17:28:15 +0100

    change array assertions to match mock dual recording size, add tests for channel separation and binning

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -1,21 +1,25 @@
 import pytest
+from click.testing import CliRunner
+
+import numpy as np
+from dask import array as da
 
 import mesoscopy.preprocess as preprocess
 import mesoscopy.preprocess.calculations as calculations
 
 
 def test_load_raw_h5(raw_h5):
-    session_id, imaging_data, timestamps = preprocess._load_raw(raw_h5, nwb=False)
+    session_id, imaging_data, timestamps = preprocess.load_raw(raw_h5, nwb=False)
     assert session_id == "preproc_test"
-    assert imaging_data.shape == (300, 142, 142)
-    assert len(timestamps) == 300
+    assert imaging_data.shape == (600, 142, 142)
+    assert len(timestamps) == 600
 
 
 def test_load_raw_nwb(nwbfile):
-    session_id, imaging_data, timestamps = preprocess._load_raw(nwbfile, nwb=True)
+    session_id, imaging_data, timestamps = preprocess.load_raw(nwbfile, nwb=True)
     assert session_id == "session_1234"
-    assert imaging_data.shape == (300, 142, 142)
-    assert len(timestamps) == 300
+    assert imaging_data.shape == (600, 142, 142)
+    assert len(timestamps) == 600
 
 
 def test_channel_qa():
@@ -23,62 +27,84 @@ def test_channel_qa():
 
 
 def test_nwb_link(nwbfile, preproc_h5):
-    nwb = preprocess._update_nwb(nwbfile, preproc_h5)
+    nwb = preprocess.update_nwb(nwbfile, preproc_h5)
     assert nwb.processing["ophys"]["DeltaFSeries"].data
     assert nwb.processing["ophys"]["DeltaFSeries"].data.shape == (300, 142, 142)
     assert nwb.processing["ophys"]["DeltaFSeries"].timestamps
 
 
-def test_preprocess_h5(raw_h5):
+def test_binning(output_dir):
+    array = calculations.bin_array(
+        array=da.random.random((300, 142, 142)),
+        bins=2,
+        interim_dir=output_dir,
+        session_id="null",
+    )
+    assert array.shape == (300, 71, 71)
+
+
+def test_separate_channels(output_dir, random_idx):
+    # Generate mock dual-channel imaging data
+    mock_gcamp = np.random.normal(70, 200, size=(300, 142, 142))
+    mock_isosb = np.random.normal(65, 50, size=(300, 142, 142))
+
+    # Merge the two channels in random order
+    mock_dual_channel = np.insert(
+        mock_gcamp, random_idx - np.arange(len(random_idx)), mock_isosb, axis=0
+    )
+
+    gcamp_filter, isosb_filter = calculations.separate_channels(
+        array=da.from_array(mock_dual_channel, chunks=(100, 142, 142)),
+        qa_dir=output_dir,
+        session_id="null",
+    )
+
+    assert mock_dual_channel[gcamp_filter].shape[0] == 300
+    assert mock_dual_channel[isosb_filter].shape[0] == 300
+    assert (np.where(isosb_filter) == random_idx).all()
+
+
+def test_preprocess_h5(raw_h5, output_dir):
+    # preprocess.run_preprocessing(
+    #     path=raw_h5, out_dir=output_dir, interim_dir=output_dir
+    # )
     ...
 
 
-def test_preprocess_h5_crop(raw_h5):
-    ...
+def test_preprocess_h5_crop(raw_h5): ...
 
 
-def test_preprocess_h5_frameskip(raw_h5):
-    ...
+def test_preprocess_h5_frameskip(raw_h5): ...
 
 
-def test_preprocess_h5_bin(raw_h5):
-    ...
-
-def test_preprocess_nwb(nwbfile):
-    ...
+def test_preprocess_h5_bin(raw_h5): ...
 
 
-def test_preprocess_nwb_crop(nwbfile):
-    ...
+def test_preprocess_nwb(nwbfile): ...
 
 
-def test_preprocess_nwb_frameskip(nwbfile):
-    ...
+def test_preprocess_nwb_crop(nwbfile): ...
 
 
-def test_preprocess_nwb_bin(nwbfile):
-    ...
+def test_preprocess_nwb_frameskip(nwbfile): ...
 
 
-def test_preprocess_nwb_channel_means(nwbfile):
-    ...
+def test_preprocess_nwb_bin(nwbfile): ...
 
 
-def test_preprocess_nwb_use_means(nwbfile):
-    ...
+def test_preprocess_nwb_channel_means(nwbfile): ...
 
 
-def test_preprocess_nwb_flip_channels(nwbfile):
-    ...
+def test_preprocess_nwb_use_means(nwbfile): ...
 
 
-def test_preprocess_nwb_use_means_flip_channels(nwbfile):
-    ...
+def test_preprocess_nwb_flip_channels(nwbfile): ...
 
 
-def test_preprocess_nwb_interim_dir(nwbfile):
-    ...
+def test_preprocess_nwb_use_means_flip_channels(nwbfile): ...
 
 
-def test_preprocess_nwb_chunksize(nwbfile):
-    ...
+def test_preprocess_nwb_interim_dir(nwbfile): ...
+
+
+def test_preprocess_nwb_chunksize(nwbfile): ...
