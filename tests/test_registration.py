35893502ba7e2fa01ba68d9209f9482d2b396752
Constantinos Eleftheriou <Constantinos.Eleftheriou@ed.ac.uk>
2025-01-27 14:37:38 +0000

    add skeleton for registration tests

---
+++ tests/test_registration.py
@@ -0,0 +1,19 @@
+import pytest
+
+import mesoscopy.register as register
+
+
+def test_landmarks_affine():
+    """Test affine transform calculation for defined landmarks"""
+
+
+def test_load_preprocessed(): ...
+
+
+def test_update_nwb(): ...
+
+
+def test_register_landmarks_cli(): ...
+
+
+def test_add_landmarks_gui(): ...
