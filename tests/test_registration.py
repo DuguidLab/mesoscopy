7bc3d8d579c9664a89709c480eccd059d4700102
Constantinos Eleftheriou <Constantinos.Eleftheriou@ed.ac.uk>
2025-03-26 12:42:13 +0000

    add test for nwb update

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -23,12 +23,12 @@ def test_load_preprocessed_nwb(preproc_nwb):
     assert ts.shape == (300,)
 
 
-# def test_update_nwb(nwbfile, preproc_h5):
-#     tform_mock = np.array([(1, 2, 3), (1, 2, 4)])
-#     nwb = register._update_nwb(nwbfile, preproc_h5, tform_mock)
-#     assert nwb.processing["ophys"]["corrected"].data
-#     assert nwb.processing["ophys"]["xy_translation"].data
-#     assert nwb.processing["ophys"]["CCFRegisteredSeries"].data
+def test_update_nwb(nwbfile, preproc_h5):
+    tform_mock = np.array([[(1, 2, 3), (1, 2, 4)]])
+    nwb = register.update_nwb(nwbfile, preproc_h5, tform_mock)
+    assert nwb.processing["ophys"]["CCFRegisteredSeries"].corrected.data
+    assert nwb.processing["ophys"]["CCFRegisteredSeries"].original.data
+    assert nwb.processing["ophys"]["CCFRegisteredSeries"].xy_translation.data.any()
 
 
 def test_register_landmarks_cli(): ...
