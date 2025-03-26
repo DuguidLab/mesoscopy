b961f9f5b08c195e379f2e94f3f740d3e5934b80
Constantinos Eleftheriou <Constantinos.Eleftheriou@ed.ac.uk>
2025-03-26 14:15:42 +0000

    match test load_nwb

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,7 +1,9 @@
 import pytest
+from click.testing import CliRunner
 
 import numpy as np
 
+import mesoscopy
 import mesoscopy.register as register
 
 
@@ -10,14 +12,14 @@ def test_landmarks_affine():
 
 
 def test_load_preprocessed_h5(preproc_h5):
-    session_id, deltaf, ts = register._load_preprocessed(preproc_h5)
+    session_id, deltaf, ts = register.load_preprocessed(preproc_h5)
     assert session_id == "preproc"
     assert deltaf.shape == (300, 40, 40)
     assert ts.shape == (300,)
 
 
 def test_load_preprocessed_nwb(preproc_nwb):
-    session_id, deltaf, ts = register._load_preprocessed(preproc_nwb, nwb=True)
+    session_id, deltaf, ts = register.load_preprocessed(preproc_nwb, nwb=True)
     assert session_id == "session_1234"
     assert deltaf.shape == (300, 40, 40)
     assert ts.shape == (300,)
@@ -34,4 +36,4 @@ def test_update_nwb(nwbfile, preproc_h5):
 def test_register_landmarks_cli(): ...
 
 
-def test_add_landmarks_gui(): ...
+def test_mark_landmarks_gui(): ...
