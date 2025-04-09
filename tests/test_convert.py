f6f869d3454d5a98f3d041c165e5ef1b3c3697c1
Constantinos Eleftheriou <Constantinos.Eleftheriou@ed.ac.uk>
2025-04-09 11:40:21 +0100

    add tests for meta readers

--- tests/test_convert.py
+++ tests/test_convert.py
@@ -1,33 +1,35 @@
 import pytest
 
-import mesoscopy.convert as convert
+import mesoscopy.convert as conv
+import mesoscopy.convert.metadata as mtd
 
 
-def test_metadata_yaml(): ...
+def test_metadata_yaml(meta_yaml):
+    expected_keys = list(mtd.DEFAULT_METADATA.keys())
+    meta = mtd.read_yaml(meta_yaml)
+    assert sorted(expected_keys) == sorted(list(meta.keys()))
+    assert meta.get("subject_id") == "testyaml"
+    assert meta.get("experimenter") == "John Doe"
 
 
-def test_metadata_yaml_partial(): ...
+def test_metadata_json(meta_json):
+    expected_keys = list(mtd.DEFAULT_METADATA.keys())
+    meta = mtd.read_json(meta_json)
+    assert sorted(expected_keys) == sorted(list(meta.keys()))
+    assert meta.get("subject_id") == "testjson"
+    assert meta.get("experimenter") == "John Doe"
 
 
-def test_metadata_json(): ...
+def test_convert_h5_linkonly(raw_h5, output_dir): ...
 
 
-def test_metadata_json_partial(): ...
+def test_convert_h5_eagercopy(raw_h5, output_dir): ...
 
 
-def test_convert_linkonly(): ...
+def test_convert_h5_metadata_args(raw_h5, output_dir): ...
 
 
-def test_convert_eagercopy(): ...
+def test_convert_h5_metadata_args_file_mixed(raw_h5, output_dir): ...
 
 
-def test_convert_metadata_args(): ...
-
-
-def test_convert_metadata_args_partial(): ...
-
-
-def test_convert_metadata_args_file_mixed(): ...
-
-
-def test_convert_cli(): ...
+def test_convert_h5_cli(raw_h5, output_dir): ...
