5c8119ef0d38f101aaab7669d4a3e6d27e2feab5
Constantinos Eleftheriou <Constantinos.Eleftheriou@ed.ac.uk>
2025-07-14 17:58:10 +0100

    add return paths to qa functions

--- src/mesoscopy/preprocess/qa.py
+++ src/mesoscopy/preprocess/qa.py
@@ -29,7 +29,7 @@ import numpy.typing as npt
 
 def plot_frame_statistics(
     frame_means: npt.NDArray, frame_stds: npt.NDArray, qa_dir: str = ".", session_id: str = "null"
-) -> None:
+) -> dict[str, str]:
     """Plot and save quality assurance (QA) statistics for frame means and standard deviations.
 
     Args:
@@ -37,6 +37,9 @@ def plot_frame_statistics(
         frame_means (npt.NDArray): Array of mean values for each frame.
         frame_stds (npt.NDArray): Array of standard deviation values for each frame.
         session_id (str, optional): Session identifier for interim path. Defaults to "null".
+
+    Returns:
+        dict[str, str]: Dictionary containing paths to the saved QA plots.
     """
     outpath = qa_dir + os.sep + session_id + "_qa_frame_means_histogram.png"
     msg = "Saved histogram for frame means at {}".format(outpath)
@@ -54,10 +57,30 @@ def plot_frame_statistics(
     msg = "Saved lineplot for frame means at {}".format(outpath)
     plots.plot_line(frame_stds, outpath, message=msg)
 
+    return {
+        "frame_means_histogram": outpath,
+        "frame_means_line": outpath,
+        "frame_stds_histogram": outpath,
+        "frame_stds_line": outpath,
+    }
+
 
 def plot_dual_channel_timeseries(
     channel_arrays: tuple[npt.NDArray, npt.NDArray], qa_dir: str = ".", session_id: str = "null"
-) -> None:
+) -> dict[str, str]:
+    """Plot and save dual channel timeseries for quality assurance (QA).
+
+    Args:
+        channel_arrays (tuple[npt.NDArray, npt.NDArray]): Tuple containing the two channel arrays.
+        qa_dir (str): Directory to save QA plots.
+        session_id (str, optional): Session identifier for interim path. Defaults to "null".
+
+    Returns:
+        dict[str, str]: Dictionary containing paths to the saved QA plots.
+    """
+    if len(channel_arrays) != 2:
+        raise ValueError("Expected a tuple of two channel arrays.")
+
     outpath = qa_dir + os.sep + session_id + "_qa_channel_means.png"
     plots.plot_lines(
         [channel_arrays[0], channel_arrays[1]],
@@ -65,15 +88,23 @@ def plot_dual_channel_timeseries(
         message="Saved channel means at {}".format(outpath),
     )
 
+    return {
+        "channel_means": outpath,
+    }
+
 
 def plot_channel_projection_images(
     channel_array: npt.NDArray, qa_dir: str = ".", session_id: str = "null", channel: str = "null"
-) -> None:
+) -> dict[str, str]:
     """Plot and save channel mean, standard deviation and maximum intensity projection frames.
     Args:
         channel_array (npt.NDArray): Array containing channel statistics.
         qa_dir (str): Directory to save QA plots.
         session_id (str, optional): Session identifier for interim path. Defaults to "null".
+        channel (str, optional): Name of the channel for labeling the plots. Defaults to "null".
+
+    Returns:
+        dict[str, str]: Dictionary containing paths to the saved QA plots.
     """
     mean_frame, std_frame, maxip = dask.compute(
         channel_array.mean(axis=0),
@@ -90,16 +121,25 @@ def plot_channel_projection_images(
     outpath = qa_dir + os.sep + session_id + "_qa_{}_maxip.png".format(channel)
     plots.plot_frame(maxip, outpath, message="Saved maxip frame at {}".format(outpath))
 
+    return {
+        f"{channel}_mean_projection": outpath,
+        f"{channel}_std_projection": outpath,
+        f"{channel}_maxip_projection": outpath,
+    }
+
 
 def plot_dual_dff_timeseries(
     dff_arrays: tuple[npt.NDArray, npt.NDArray], qa_dir: str = ".", session_id: str = "null"
-) -> None:
+) -> dict[str, str]:
     """Plot and save dual channel DFF timeseries.
 
     Args:
         dff_arrays (tuple[npt.NDArray, npt.NDArray]): Tuple containing the ∆F for each channel.
         qa_dir (str): Directory to save QA plots.
         session_id (str, optional): Session identifier for interim path. Defaults to "null".
+
+    Returns:
+        dict[str, str]: Dictionary containing paths to the saved QA plots.
     """
     outpath = qa_dir + os.sep + session_id + "_qa_dual_dff_timeseries.png"
     plots.plot_lines(
@@ -108,26 +148,44 @@ def plot_dual_dff_timeseries(
         message="Saved dual channel DFF timeseries at {}".format(outpath),
     )
 
+    return {
+        "dual_dff_timeseries": outpath,
+    }
 
-def plot_f_example(f_signal: npt.NDArray, qa_dir: str = ".", session_id: str = "null") -> None:
+
+def plot_f_example(f_signal: npt.NDArray, qa_dir: str = ".", session_id: str = "null") -> dict[str, str]:
     """Plot and save an example frame of the F signal.
 
     Args:
         f_signal (npt.NDArray): Array containing the F signal.
         qa_dir (str): Directory to save QA plots.
         session_id (str, optional): Session identifier for interim path. Defaults to "null".
+
+    Returns:
+        dict[str, str]: Dictionary containing paths to the saved QA plots.
     """
     outpath = qa_dir + os.sep + session_id + "_qa_f_example.png"
     plots.plot_frame(f_signal[200], outpath, message="Saved F signal example at {}".format(outpath))
 
+    return {
+        "f_example": outpath,
+    }
 
-def plot_mean_f_timeseries(f_signal: npt.NDArray, qa_dir: str = ".", session_id: str = "null") -> None:
+
+def plot_mean_f_timeseries(f_signal: npt.NDArray, qa_dir: str = ".", session_id: str = "null") -> dict[str, str]:
     """Plot and save the mean F signal timeseries.
 
     Args:
         f_signal (npt.NDArray): Array containing the F signal.
         qa_dir (str): Directory to save QA plots.
         session_id (str, optional): Session identifier for interim path. Defaults to "null".
+
+    Returns:
+        dict[str, str]: Dictionary containing paths to the saved QA plots.
     """
     outpath = qa_dir + os.sep + session_id + "_qa_mean_f_timeseries.png"
     plots.plot_line(f_signal.mean(axis=(1, 2)), outpath, message="Saved mean F timeseries at {}".format(outpath))
+
+    return {
+        "mean_f_timeseries": outpath,
+    }
