0f9605b7d5caff5ecb464d012403a498c7b2d517
Constantinos Eleftheriou <constantinos.eleftheriou@ed.ac.uk>
2026-09-18 12:33:57 +0100

    add preprocessing imports to expensive imports exclusion test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -36,6 +36,7 @@ HEAVY_MODULES = ["napari", "sklearn", "scipy", "pims", "matplotlib", "pynwb", "d
 # `scipy` itself is cheap and comes in through dask, `scipy.stats` is not.
 STAGE_HEAVY_MODULES = {
     "process": ["napari", "sklearn", "scipy.stats", "scipy.ndimage", "pims", "matplotlib", "plotly", "diptest"],
+    "preprocess": ["napari", "sklearn", "scipy.stats", "pims", "matplotlib", "plotly", "diptest"],
     "register": ["napari", "magicgui", "skimage", "sklearn", "scipy.stats", "pims", "matplotlib", "plotly", "diptest"],
     "report": ["napari", "magicgui", "skimage", "sklearn", "scipy.stats", "pims", "matplotlib", "plotly", "diptest"],
 }
